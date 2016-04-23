8cffa0f156bc81b293fed97aa948450c353913f7
mereddyHarshitha <mereddyHarshitha@missionrnd.com>
2016-04-24 00:21:12 +0530

    Submit Commit

--- src/Maze.cpp
+++ src/Maze.cpp
@@ -35,8 +35,62 @@ more parameters .
 
 #include<stdlib.h>
 
+int find_path(int *maze, int rows, int columns, int x1, int y1, int x2, int y2){
+
+	if (x1 >= rows || y1 >= columns || x2 >= rows || y2 >= columns)
+		return 0;
+
+	int xdiff, ydiff,temp=0;
+	
+	if (x1 - x2 < 0)
+		xdiff = x2 - x1;
+	else
+		xdiff = x1 - x2;
+
+	if (y1 - y2 < 0)
+		ydiff = y2 - y1;
+	else
+		ydiff = y1 - y2;
+
+	int p = maze[x1*columns + y1];
+	int q = maze[x2*columns + y2];
+
+	if (p == 1 && q == 1){
+		if (xdiff == 0 && ydiff == 1 || xdiff == 1 && ydiff == 0)
+			temp = 1;
+	}
+
+	return temp;
+}
+int path_exists_wrapper(int *maze, int rows, int columns, int x1, int y1, int x2, int y2,int position){
+
+	if (find_path(maze, rows, columns, x1, y1, x2, y2))
+		return 1;
+
+	int right = find_path(maze, rows, columns, x1, y1, x1, y1 + 1);
+	int left = find_path(maze, rows, columns, x1, y1, x1, y1 - 1);
+	int top = find_path(maze, rows, columns, x1, y1, x1 - 1, y1);
+	int down = find_path(maze, rows, columns, x1, y1, x1 + 1, y1);
+
+	if ( right && position != 3)
+		return path_exists_wrapper(maze, rows, columns, x1, y1 + 1, x2, y2, 1);
+
+	if ( down && position != 4)
+		return path_exists_wrapper(maze, rows, columns, x1+1, y1, x2, y2, 2);
+
+	if ( left && position != 1)
+		return path_exists_wrapper(maze, rows, columns, x1, y1-1, x2, y2, 3);
+
+	if ( top && position != 2)
+		return path_exists_wrapper(maze, rows, columns, x1-1, y1 , x2, y2, 4);
+
+	return 0;
+}
 
 int path_exists(int *maze, int rows, int columns, int x1, int y1, int x2, int y2)
 {
-	return 1;
+	if (rows < 0 || columns < 0|| x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0)
+		return 0;
+
+	return path_exists_wrapper(maze,rows,columns,x1,y1,x2,y2,0);
 }
--- src/Spiral.cpp
+++ src/Spiral.cpp
@@ -34,7 +34,60 @@ Note : Check the function Parameters ,Its a double pointer .
 #include "stdafx.h"
 #include<stdlib.h>
 
+
+int *show_spiral(int **input_array, int *index, int *res_array, int *r_strt, int *r_end, int *c_strt, int *c_end){
+
+	int p = *index;
+	int i;
+	int a = *r_strt;
+	int b = *c_strt;
+	int c = *r_end;
+	int d = *c_end;
+
+	for (i = b; i < d; ++i){
+		res_array[p++] = input_array[a][i];
+	}
+	a++;
+
+	for (i = a; i < c; ++i){
+		res_array[p++] = input_array[i][d - 1];
+	}
+	d--;
+
+	if (a < c){
+		for (i = d - 1; i >= b; --i){
+			res_array[p++] = input_array[c - 1][i];
+		}
+		c--;
+	}
+
+	if (b < d){
+		for (i = c - 1; i >= a; --i){
+			res_array[p++] = input_array[i][b];
+		}
+		b++;
+	}
+
+	if (a < c && b < d)
+		return show_spiral(input_array, &p, res_array, &a, &c, &b, &d);
+	else
+		return res_array;
+}
+
 int *spiral(int rows, int columns, int **input_array)
 {
-	return NULL;
+	if (rows <= 0 || columns <= 0 || input_array == NULL)
+		return NULL;
+
+	int *res_array = (int*)calloc(rows*columns, sizeof(int));
+
+	int index = 0;
+	int r_strt = 0, c_strt = 0;
+	int r_end = rows;
+	int c_end = columns;
+
+	res_array = show_spiral(input_array, &index, res_array, &r_strt, &r_end, &c_strt, &c_end);
+
+	return res_array;
+	
 }
